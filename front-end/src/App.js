35a1a39e4aab68c3b2a0e8126241dac096b6f3f7
Ericawanja <wanjaerica@gmail.com>
2022-12-01 15:07:02 +0300

    layout design

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,26 @@
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import "./components/styles.css";
 
-import './App.css';
-import LandingPage from './components/LandingPage';
-import  './components/styles.css';
-
-import { Route, Routes } from "react-router-dom"
-import Login from './components/Login';
-import Signup from './components/Signup';
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import QuestionLayout from "./components/QuestionLayout";
+import QuestionsList from "./components/QuestionsList";
+import Question from "./components/Question";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={ <LandingPage/>}/>
-      <Route path='/login' element={ <Login/>}/>
-      <Route path='/signup' element={ <Signup/>}/>
-    </Routes>
-   
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/questions" element={<QuestionLayout />}>
+          <Route index element={<QuestionsList />} />
+          <Route path=":question_id" element={<Question />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- front-end/src/components/QuestionLayout.jsx
+++ front-end/src/components/QuestionLayout.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Outlet, NavLink } from "react-router-dom";
+import {IconContext} from 'react-icons'
+import {FaSearch} from 'react-icons/fa'
 
 function QuestionLayout() {
   return (
-    <div>QuestionLayout</div>
-  )
+    <div className="layout-container">
+      <div className="layout_wrapper">
+        <div className="layout_header">
+          <div className="logo">Stack Overflow-Lite</div>
+          <div className="search">
+            <span className="search_input"><input type="text" /></span>
+            <span className="search_btn">
+              <IconContext.Provider value={{color: "white", size: "20px" }}>
+                <FaSearch/>
+              </IconContext.Provider>
+            </span>
+            </div>
+            <div className="layout_logout">
+                Log Out
+            </div>
+          
+        </div>
+        <div className="layout_body">
+          <div className="layout_aside">
+            <div className="layout_aside_inner">
+             <div><NavLink to="/questions">Home</NavLink></div> 
+              <div className="quiz_links">
+                <span>Questions</span>
+                <div><NavLink>All</NavLink></div>
+                <div><NavLink>Recent</NavLink></div>
+                <div><NavLink>Answered</NavLink></div>
+                <div><NavLink>Unaswered</NavLink></div>
+                <div><NavLink>My questions</NavLink></div>
+              </div>
+              <div><NavLink>Tags</NavLink></div>
+              <div><NavLink>Profile</NavLink></div>
+            </div>
+          </div>
+          <div className="layout_main">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default QuestionLayout
+export default QuestionLayout;
