539a692f77ccc1502ef3d35d7094da72d7b55a9a
Ericawanja <wanjaerica@gmail.com>
2022-12-02 12:00:38 +0300

    Add answer component

--- front-end/src/components/question/addAnswer/index.jsx
+++ front-end/src/components/question/addAnswer/index.jsx
@@ -1,8 +1,20 @@
-function AddAnswer(){
-    return(
-        <div>
-           Form to add answer  
-        </div>
-    )
+function AddAnswer() {
+  return (
+    <div className="form_answer_container">
+      <h3>Add a answer</h3>
+      <div className="answer_form">
+        <textarea
+          name="answer"
+          id=""
+          cols="50"
+          rows="5"
+          placeholder="Answer..."
+        ></textarea>
+      </div>
+      <div className="answer_save_btn">
+        <button>save</button>
+      </div>
+    </div>
+  );
 }
-export default AddAnswer
+export default AddAnswer;
--- front-end/src/services/question.service.js
+++ front-end/src/services/question.service.js
@@ -1,5 +1,7 @@
 //logic for api calls used in the thunks
 
+
+
 // import { awaitPromise } from ".";
 
 class QuestionService {
@@ -10,24 +12,28 @@ class QuestionService {
       const response = await fetch(this.BASE_URL);
       const data = await response.json();
 
+      
+
       return { data, error: null };
     } catch (error) {
       return { data: null, error: error.message };
     }
   }
 
-  async GetOneQuestion(questionId) {
+  async GetOneQuestion(questionId){
     try {
-      const response = await fetch(this.BASE_URL + "/" + questionId);
-      const data = await response.json();
+        const response = await fetch(this.BASE_URL+"/"+questionId);
+        const data = await response.json();
 
-      //
-
-      return { data, error: null };
-    } catch (error) {
-      return { data: null, error: error.message };
-    }
+        // 
+  
+        return { data, error: null };
+      } catch (error) {
+        return { data: null, error: error.message };
+      }
   }
+
+
 }
 
 export default new QuestionService();
