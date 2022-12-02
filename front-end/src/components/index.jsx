e8d2f6381a2d1ab0ab123e4d4601f0abb6cd2c85
Ericawanja <wanjaerica@gmail.com>
2022-12-02 08:29:28 +0300

    folder structure and mockup database

--- front-end/src/components/index.jsx
+++ front-end/src/components/index.jsx
@@ -1,2 +1,3 @@
 export {default as QuestionsList} from "./questionList1"
 export {default as Question} from "./question"
+export {default as QuestionLayout} from "./common/questionsLayout"
--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -1,9 +1,11 @@
 //handling all the exports to the app.js
 
 
-export {default as QuestionsPage} from "./questionsexample"
+//export {default as QuestionsPage} from "./questionsexample"
 export {default as LandingPage} from "./home"
 export {default as LoginPage} from "./login"
 export {default as SignupPage} from "./signup"
 export {default as QuestionsLayoutPage} from "./questionsLayoutPage"
+export {default as QuestionsPage} from "./questions"
+export {default as QuestionPage} from "./question"
 
