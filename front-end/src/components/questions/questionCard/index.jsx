ca027fc41cfcfe1fba4cc6c4ba3bd705a8010d05
Ericawanja <wanjaerica@gmail.com>
2022-12-19 22:57:44 +0300

    formated date

--- front-end/src/components/questions/questionCard/index.jsx
+++ front-end/src/components/questions/questionCard/index.jsx
@@ -3,32 +3,24 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setIsEditingTrue } from "../../../redux/slices/question.slice";
 
-import {
-  deleteQuestion,
+import { formatDistance, subDays } from "date-fns";
 
-} from "../../../redux/thunks/question.thunks";
+import { deleteQuestion } from "../../../redux/thunks/question.thunks";
 
-function QuestionCard({ single_question, currentUser ={} }) {
+function QuestionCard({ single_question, currentUser = {} }) {
   const dispatch = useDispatch();
   let { question_id, user_id, title, question, tags, created_on, answers } =
     single_question;
   let sub_string = question.slice(0, 130);
   let tagsArr = tags.split(",");
 
-
-
-  let createdOn = created_on
-    ? new Date(created_on).toLocaleDateString("en-US", {
-        weekday: "long",
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-      })
-    : created_on;
-
+  
+  let date = formatDistance(new Date(created_on), new Date(), {
+    addSuffix: true,
+  });
+  console.log(date);
   const navigate = useNavigate();
   const navigateToQuestion = () => {
-   
     navigate(`/questions/${question_id}`);
   };
 
@@ -41,7 +33,9 @@ function QuestionCard({ single_question, currentUser ={} }) {
         <div className="quiz_title" onClick={navigateToQuestion}>
           <span>{title}</span>
         </div>
-        <div className="quiz_description" onClick={navigateToQuestion}>{sub_string}...</div>
+        <div className="quiz_description" onClick={navigateToQuestion}>
+          {sub_string}...
+        </div>
         <div className="quiz_list_details">
           <div className="tags">
             {tagsArr.map((tag, index) => {
@@ -53,9 +47,9 @@ function QuestionCard({ single_question, currentUser ={} }) {
               <span className="author">
                 {/* <AiOutlineUser style={{ fontSize: "16px" }} />
                 {username} */}
-                Posted on:
+                Posted
               </span>
-              <span className="date_created">{createdOn}</span>
+              <span className="date_created">{date}</span>
             </div>
           ) : (
             <div>
@@ -74,7 +68,7 @@ function QuestionCard({ single_question, currentUser ={} }) {
                     <AiFillDelete style={{ fontSize: "16px" }} />
                   </span>
                 </span>
-                <span className="date_created">{createdOn}</span>
+                <span className="date_created">{date}</span>
               </div>
             </div>
           )}
